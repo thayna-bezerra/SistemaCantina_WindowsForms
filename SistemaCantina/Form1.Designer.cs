09e0698e22d7884166159beb0a06690ef36e237d
blequekakarotto <arraisthayna2106@gmail.com>
2021-09-24 07:48:06 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaCantina/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace SistemaCantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fotinha = new System.Windows.Forms.PictureBox();
+            this.lstCaixa = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fotinha)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(212, 82);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(68, 23);
+            this.txtCodigo.TabIndex = 0;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(409, 359);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "R$ 0,00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Reem Kufi", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(43, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CÓDIGO DO PRODUTO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Reem Kufi", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(297, 359);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "VALOR TOTAL:";
+            // 
+            // fotinha
+            // 
+            this.fotinha.ImageLocation = "";
+            this.fotinha.Location = new System.Drawing.Point(43, 116);
+            this.fotinha.Name = "fotinha";
+            this.fotinha.Size = new System.Drawing.Size(237, 229);
+            this.fotinha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fotinha.TabIndex = 4;
+            this.fotinha.TabStop = false;
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.FormattingEnabled = true;
+            this.lstCaixa.ItemHeight = 15;
+            this.lstCaixa.Location = new System.Drawing.Point(297, 86);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(187, 259);
+            this.lstCaixa.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(140, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(235, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "SISTEMA CANTINA";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.White;
+            this.btnLimpar.FlatAppearance.BorderSize = 0;
+            this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpar.Font = new System.Drawing.Font("Reem Kufi", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnLimpar.Location = new System.Drawing.Point(174, 359);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(90, 37);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.White;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSair.Font = new System.Drawing.Font("Reem Kufi", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSair.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnSair.Location = new System.Drawing.Point(60, 359);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(90, 37);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(535, 427);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.fotinha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigo);
+            this.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "cantina";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fotinha)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox fotinha;
+        private System.Windows.Forms.ListBox lstCaixa;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ SistemaCantina/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaCantina
+{
+    public partial class Form1 : Form
+    {
+        string[] produtos = new string[10];
+        string[] codigo = new string[10];
+        double[] valor = new double[10];
+        double soma;
+        // arrays para armazenar 6 produtos -- códigos e valores
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            //se o usuário digitar um código com 3 caracteres (length), esse código é adicionado ao listBox. 
+            if (txtCodigo.Text.Length == 3) //length retorna a quantidade de caracteres
+            {
+                int indice = 0;
+                for (int prod = 1; prod < codigo.Length; prod++) // enquanto o prod for menor ou igual a qtde de itens do array 
+                {
+                    if(txtCodigo.Text == codigo[prod])
+                    {
+                        indice = prod;
+                    }
+                }
+
+                if (indice == 0)
+                {
+                    MessageBox.Show("Produto não encontrado", "Tente Novamente", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    // se foi encontrado o produto ele retorna as informações de acordo com a posição na array
+                }
+                else
+                {
+                    lstCaixa.Items.Add(txtCodigo.Text + " -- " + produtos[indice] + "-- R$ " + valor[indice]); //add na listbox
+                    soma = soma + valor[indice];
+                    label1.Text = ("R$ " + soma);
+
+                    fotinha.ImageLocation = "c:/imagens/" + codigo[indice] + ".jpg";//localização da imagem
+                    txtCodigo.Text = "";//o textbox será limpo
+                    txtCodigo.Focus();//Focus posiciona o cursor para o TextBox para uma nova digitação
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carregarArray();
+            soma = 0;
+        }
+
+        private void carregarArray()
+        {
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+            codigo[5] = "005";
+
+            produtos[1] = "Pastel";
+            produtos[2] = "Coxinha";
+            produtos[3] = "Hot Dog";
+            produtos[4] = "Chocolate";
+            produtos[5] = "Suco";
+
+            valor[1] = 6.00;
+            valor[2] = 5.00;
+            valor[3] = 12.00;
+            valor[4] = 3.50;
+            valor[5] = 8.00;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lstCaixa.Items.Clear(); 
+            fotinha.ImageLocation = "";
+            //limpar a lista
+            //limpar foto
+        }
+    }
+}
